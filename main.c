788be2b69c3e5c24991b8f22591406d6ce1560e5
Дмитрий Туминов <d.tuminov1@outlook.com>
2021-10-31 09:15:45 +0300

    project with no doxygen

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "header.h"
+#include "onegin.h"
 
 int main(){
-    FILE *file = fopen("txt_file.txt","r");
+    FILE *file = fopen("onigin.txt","r");
     size_t lines_count = count_lines(file);
     char** strings = (char**)calloc(sizeof(*strings),lines_count);
     char buf[1024] = "";
@@ -17,8 +17,8 @@ int main(){
     for (size_t y = 0; y < lines_count; y++){
         printf("%s\n",strings[y]);
     }
-    qsort(strings, sizeof(*strings), lines_count, compar);
-    printf("after sorting:");
+    qsort(strings, lines_count,sizeof(char*), compar);
+    printf("after sorting:\n");
     for (size_t y = 0; y < lines_count; y++){
         printf("%s\n",strings[y]);
     }
---
+++ onegin.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int mystrcmp(const char *first, const char *second);
+int compar (const void *pl1, const void* pl2);
+size_t count_lines(FILE* file);
